9a9a8339964886f43cddba518e42307f05524ea5
agilitix <adsbox@outlook.fr>
2019-11-02 11:08:43 +0100

    Added sample, to be continued

---
+++ NLuaSample/Sample/Sample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLua;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var lua = new Lua())
+            {
+                lua.State.Encoding = Encoding.UTF8;
+                lua.LoadCLRPackage();
+
+                lua.DoString("print ('Hello world!')");
+            }
+
+            Console.WriteLine();
+            Console.Write("Exit:");
+            Console.ReadKey();
+        }
+    }
+}
